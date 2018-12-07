c8913047760e0c78d75f6ce95b82151e7d6f4cff
emyhoang <emyhoang8899@gmail.com>
2018-12-07 10:24:28 -0800

    added decode method, additional solution commented out, and tested the solutions.

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -3,10 +3,19 @@
 // For example we can represent the original 53 characters with only 13.
 
 export const encode = (str) => {
-  let consecutiveChars = /([a-zA-Z])\1*/g;
-  return str.replace(consecutiveChars,
+  let cChars = /([\w\s])\1*/g;
+  return str.replace(cChars,
     match => (match.length > 1 ? match.length + match[0] : match[0]));
 };
 
 export const decode = (encodedText) => {
+  let countAndChar = /(\d+)(\w|\s)/g;
+  return encodedText.replace(countAndChar,
+    (match, repeats, char) => new Array(+repeats + 1).join(char));
 };
+
+// Another way to solve:
+  // const RLE = {
+  //   encode: str => str.replace(/(\w)\1+/g, (m, c) => `${m.length}${c}`),
+  //   decode: str => str.replace(/(\d+)(\w)/g, (_, d, c) => c.repeat(d))
+  // }
