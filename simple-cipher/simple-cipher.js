e53be3da51d4a25ae96f82ae961e29932501d53d
emyhoang <emyhoang8899@gmail.com>
2018-12-10 15:13:22 -0800

    function to convert numbers to string and tested numbers 1,3,5,7

---
+++ simple-cipher/simple-cipher.js
@@ -0,0 +1,17 @@
+// Implement a simple shift cipher like Caesar and a more secure substitution cipher.
+
+export const Cipher = (input) => {
+
+var map = {
+  a: 'q', b: 'w', c: 'e',
+  d: 'r', e: 't', f: 'y',
+  g: 'u', h: 'i', i: 'o',
+  j: 'p', k: 'a', l: 's',
+  m: 'd', n: 'f', o: 'g',
+  p: 'h', q: 'j', r: 'k',
+  s: 'l', t: 'z', u: 'x',
+  v: 'c', w: 'v', x: 'b',
+  y: 'n', z: 'm'
+}
+
+}
