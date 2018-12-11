a3b0758ccd1117221594bb87fb42b7b4a83eb5fa
emyhoang <emyhoang8899@gmail.com>
2018-12-10 16:24:39 -0800

    tested transforming more values & keys

--- etl/etl.spec.js
+++ etl/etl.spec.js
@@ -8,7 +8,7 @@ describe('Transform', () => {
     expect(transform(old)).toEqual(expected);
   });
 
-  xtest('transforms more values', () => {
+  test('transforms more values', () => {
     const old = { 1: ['A', 'E', 'I', 'O', 'U'] };
     const expected = {
       a: 1, e: 1, i: 1, o: 1, u: 1,
@@ -17,7 +17,7 @@ describe('Transform', () => {
     expect(transform(old)).toEqual(expected);
   });
 
-  xtest('transforms more keys', () => {
+  test('transforms more keys', () => {
     const old = { 1: ['A', 'E'], 2: ['D', 'G'] };
     const expected = {
       a: 1, e: 1, d: 2, g: 2,
